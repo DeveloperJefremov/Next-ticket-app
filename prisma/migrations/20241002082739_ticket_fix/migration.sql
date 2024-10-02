b06aaf87aaabb29e2c9fc2ed45d6a41229045d28
Artjoms Jefremovs <developerjefremov@gmail.com>
2024-10-02 17:56:33 +0300

    fixes

--- prisma/migrations/20241002082739_ticket_fix/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `assiignedToUserId` on the `ticket` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE `ticket` DROP COLUMN `assiignedToUserId`,
-    ADD COLUMN `assignedToUserId` INTEGER NULL;
